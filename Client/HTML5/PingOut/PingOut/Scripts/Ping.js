69a4bd65f7d59cb2c994ca058b20e3a79637a124
K.Siva Senthil <ksivasenthil@gmail.com>
2015-01-30 06:38:41 +0530

    Added client structure + template for client
    Added the basic template, and style to the list page of the client.

--- Client/HTML5/PingOut/PingOut/Scripts/Ping.js
+++ Client/HTML5/PingOut/PingOut/Scripts/Ping.js
@@ -1,1 +1,18 @@
-﻿
+﻿$(document).ready(function () {
+    var data = {
+        "pings": [
+            {
+                "Source": "1",
+                "last_message": "1 list ..."
+            },
+            {
+                "Source": "2",
+                "last_message": "2 list dynamic ..."
+            }
+        ]
+
+    };
+    var template = $(templateHost).html();
+    var formattedMarkup = Mustache.render(template, data);
+    $('#contentHost').html(formattedMarkup);
+});
